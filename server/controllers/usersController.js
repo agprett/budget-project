f07546adf3fd20aa0f6d3e8ffccf79dfa405a86d
Alec Prettyman <alecprettyman@alecs-mbp.lan>
2020-04-29 11:04:15 -0600

    nav setup

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -2,7 +2,7 @@ module.exports = {
   getUser: async (req, res) => {
     db = req.app.get('db')
 
-    const user = await db.users.get_user()
+    const [user] = await db.users.get_user()
 
     res.status(200).send(user)
   }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -4,6 +4,7 @@ import './Nav.css'
 import {withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {getUser} from '../../ducks/reducer'
+import {home, budget, friends, groups, logout} from './img.json'
 
 function Nav(props){
 
@@ -13,12 +14,41 @@ function Nav(props){
       props.getUser(res.data)
     })
     .catch(err => console.log(err))
-  })
+  }, [])
   
   return (
     <div className='nav-bar'>
-      <div className='prof-pic'></div>
-      <p>{props.user.username}</p>
+      <div className='nav-prof'>
+        <div className='prof-pic'></div>
+        <p>{props.user.username}</p>
+      </div>
+      <img
+        className='nav-button'
+        src={home}
+        alt='home'
+        onClick={() => props.history.push('/dash')}
+      />
+      <img
+        className='nav-button'
+        src={budget}
+        alt='budget'
+        onClick={() => props.history.push('/budget')}
+      />
+      <img
+        className='nav-button'
+        src={friends}
+        alt='friends'
+      />
+      <img
+        className='nav-button'
+        src={groups}
+        alt='groups'
+      />
+      <img
+        className='nav-logout'
+        src={logout}
+        alt='logout'
+      />
     </div>
   )
 }
