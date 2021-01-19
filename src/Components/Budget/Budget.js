9c3dd71789d64576c10ae3f7cd25f900b1e55478
Alec Prettyman <prettale4@gmail.com>
2021-01-18 22:14:55 -0700

    connected the budget page to the db

--- src/Components/Budget/Budget.js
+++ src/Components/Budget/Budget.js
@@ -1,8 +1,55 @@
-import React from 'react'
-// import axios from 'axios'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import './Budget.css'
 
 function Budget(){
+  const [budget, setBudget] = useState([])
+  const [current, setCurrent] = useState([])
+
+  useEffect(() => {
+    axios.get('api/expenses/current')
+      .then(res => {
+        setCurrent(res.data)
+      })
+      .catch(err => console.log(err))
+
+    axios.get('api/budget')
+      .then(res => {
+        setBudget(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  const spent = (category) => {
+    let amount
+
+    for(let i = 0; i < current.length; i++){
+      if(current[i].category === category){
+        return amount = (current[i].amount ? current[i].amount : 0)
+      }
+      amount = 0
+    }
+
+    return amount
+  }
+
+  const viewSubs = budget.map((budget, i) => {
+    const {category, amount} = budget
+
+    return (
+      <section className='sub-budget' key={i}>
+        <p>{category}:</p>
+        <p>${spent(category)} / ${amount}</p>
+        <div className='amount-bar'>
+          <div className='spent-bar'
+            style={{width: `${spent(category)/amount*100}%`}}
+          >
+          </div>
+        </div>
+      </section>
+    )
+  })
+
   return (
     <section className='budget-page'>
       <div className='budget-left'>
@@ -12,7 +59,9 @@ function Budget(){
         </div>
         <div className='budget-pie-chart'>Pie Chart</div>
       </div>
-      <div className='sub-budgets'>Sub Budgets</div>
+      <div className='sub-budgets'>
+        {viewSubs}
+      </div>
     </section>
   )
 }
