5c2e37f4747f28812bdb8639e0293fa330663cf2
Alec Prettyman <prettale4@gmail.com>
2021-02-04 19:33:33 -0700

    set loading animation for expenses page

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import moment from 'moment'
+import Loading from '../Loading/Loading'
 import './Expenses.css'
 
 function Expenses() {
@@ -70,71 +71,78 @@ function Expenses() {
   })
 
   return (
-    <section className='expense-section'>
-        <section className='expense-left'>
-          <div className='new-expense'>
-            <input
-              placeholder='Name'
-              value={newExpense.name}
-              onChange={event => {
-                setNewExpense({...newExpense, name: event.target.value})
+    <section>
+      {loading ? (
+        <div className='loading'>
+          <Loading/>
+        </div>
+      ) : (
+        <section className='expense-section'>
+          <section className='expense-left'>
+            <div className='new-expense'>
+              <input
+                placeholder='Name'
+                value={newExpense.name}
+                onChange={event => {
+                  setNewExpense({...newExpense, name: event.target.value})
+                }}
+              />
+              <input
+                placeholder='Category'
+                value={newExpense.category}
+                onChange={event => {
+                  setNewExpense({...newExpense, category: event.target.value})
+                }}
+              />
+              <input
+                placeholder='Amount'
+                value={newExpense.amount}
+                onChange={event => {
+                  setNewExpense({...newExpense, amount: +event.target.value})
+                }}
+              />
+              <button
+                onClick={() => {
+                  addNewExpense(newExpense)
+                }}
+              >Submit</button>
+              <button
+                onClick={() => {
+                  setNewExpense({name: '', category: '', amount: 0})
               }}
-            />
-            <input
-              placeholder='Category'
-              value={newExpense.category}
-              onChange={event => {
-                setNewExpense({...newExpense, category: event.target.value})
-              }}
-            />
-            <input
-              placeholder='Amount'
-              value={newExpense.amount}
-              onChange={event => {
-                setNewExpense({...newExpense, amount: +event.target.value})
-              }}
-            />
-            <button
-              onClick={() => {
-                addNewExpense(newExpense)
-              }}
-            >Submit</button>
-            <button
-              onClick={() => {
-                setNewExpense({name: '', category: '', amount: 0})
-             }}
-            >Cancel</button>
-          </div>
-        </section>
-        <section className='expense-right'>
-          <section className='expense-filter'>
-            <input
-              placeholder='Name'
-            />
-            <input 
-              placeholder='Category'
-            />
-            <input
-              placeholder='Start Date'
-            />
-            <input
-              placeholder='End Date'
-            />
-            <input
-              placeholder='Minimum'
-            />
-            <input
-              placeholder='Maximum'
-            />
+              >Cancel</button>
+            </div>
           </section>
-          <div className='expenses-view'>
-            {expenses[0] ? viewExpenses : <div style={{textAlign: 'center'}}>No Expenses to Show</div>}
-          </div>
+          <section className='expense-right'>
+            <section className='expense-filter'>
+              <input
+                placeholder='Name'
+              />
+              <input 
+                placeholder='Category'
+              />
+              <input
+                placeholder='Start Date'
+              />
+              <input
+                placeholder='End Date'
+              />
+              <input
+                placeholder='Minimum'
+              />
+              <input
+                placeholder='Maximum'
+              />
+            </section>
+            <div className='expenses-view'>
+              {expenses[0] ? viewExpenses : <div style={{textAlign: 'center'}}>No Expenses to Show</div>}
+            </div>
+          </section>
+          <section className='recurring'>
+            {viewRecurring}
+          </section>
         </section>
-        <section className='recurring'>
-          {viewRecurring}
-        </section>
-
+      )}
     </section>
   )
 }
