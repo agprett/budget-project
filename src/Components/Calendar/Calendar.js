60de297d20dd9a509761d2245ae48faa72b67ed8
Alec Prettyman <prettale4@gmail.com>
2021-06-15 23:10:10 -0600

    displaying days of the month added to the calendar

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -3,11 +3,22 @@ import dayjs from 'dayjs'
 import './Calendar.css'
 
 function Calendar(){
+  const [currentDate, setCurrentDate] = useState('')
   const [startDisplayMonth, setStartDisplayMonth] = useState('')
+  const [selectedDate, setSelectedDate] = useState('')
+  const [datesArray, setDatesArray] = useState([])
 
   useEffect(() => {
+    setCurrentDate(dayjs().format('MM/DD/YY'))
     setStartDisplayMonth(dayjs().startOf('month').format('MM/DD/YY'))
   }, [])
+  
+  useEffect(() => {
+    setDatesArray([])
+    for(let i = 1; i <= +dayjs(dayjs(startDisplayMonth)).endOf('month').format('DD'); i++){
+      setDatesArray(datesArray => [...datesArray, i])
+    }
+  }, [startDisplayMonth])
 
   const handleUpdateDisplayMonth = (direction) => {
     let date = startDisplayMonth
@@ -17,8 +28,29 @@ function Calendar(){
       date = dayjs(date).add(1, 'month')
     }
 
+    setCurrentDate(dayjs().format('MM/DD/YY'))
     setStartDisplayMonth(dayjs(date).startOf('month').format('MM/DD/YY'))
   }
+
+  const designCalendar = datesArray.map((date, i) => {
+    let space = 0
+    if(date === 1){
+      space = dayjs(startDisplayMonth).get('d')
+    }
+    
+    return (
+      <div
+        className='calendar-days' 
+        key={i}
+        style={{marginLeft: `${space * 25}px`}}
+        onClick={() => {
+          console.log()
+        }}
+      >
+        {date}
+      </div>
+    )
+  })
 
   return (
     <section className='calendar'>
@@ -38,7 +70,9 @@ function Calendar(){
           setStartDisplayMonth(dayjs().startOf('month').format('MM/DD/YY'))
         }}
       >Today</button>
-      <div>{startDisplayMonth}</div>
+      <section className='calendar-month'>
+        {designCalendar}
+      </section>
     </section>
   )
 }
